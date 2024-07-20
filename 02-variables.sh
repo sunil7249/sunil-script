85a735c4ea10a05e410bbcc0a3dcc686c95772e0
sunil7249 <devops.bsg@gmail.com>
2024-07-20 20:18:01 +0530

    2nd script

--- 02-variables.sh
+++ 02-variables.sh
@@ -4,7 +4,7 @@ PERSON1=Ramesh
 PERSON2=Suresh
 
 
-echo "$PERSON1: hello $PERSON2,good morining"
-echo  "$PERSON2: hi $PERSON1, very good morning"
-echo  "$PERSON1: how are you $PERSON2?"
-echo  "$PERSON2: I am good $PERSON1, what about you"
+echo  "$PERSON1:  hello $PERSON2,good morining"
+echo  "$PERSON2:  hi $PERSON1, very good morning"
+echo  "$PERSON1:  how are you $PERSON2?"
+echo  "$PERSON2:  I am good $PERSON1, what about you"
