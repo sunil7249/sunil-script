f769773a07e6ac4f4f6cd46232e199b1b3e02671
sunil7249 <devops.bsg@gmail.com>
2024-07-20 21:51:38 +0530

    3rd script

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,7 +2,8 @@
 
 echo "Please enter your username:: "
 read -s USERNAME
+
 echo "Please enter your password:: "
-echo -s PASSWORD
+read -s PASSWORD
 
 echo "Username is: $USERNAME, Password is: $PASSWORD "
