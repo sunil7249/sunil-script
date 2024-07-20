13e282c783d5c81b4cb119a16ec5571056b73b03
sunil7249 <devops.bsg@gmail.com>
2024-07-20 20:01:26 +0530

    2nd script

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "suresh: hello ramesh, good morining"
+echo  "ramesh: hi suresh, very good morning"
+echo  "suresh: how are you ramesh?"
+echo  "ramesh: I am good suresh, what about you"
