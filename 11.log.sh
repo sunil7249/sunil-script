a21ca21fe2431a21aa2c3323188b19f6ad6632e1
sunil7249 <devops.bsg@gmail.com>
2024-07-21 15:57:25 +0530

    conditions

--- 11.log.sh
+++ 11.log.sh
@@ -4,24 +4,27 @@ ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 VALIDATE(){
 
   if [ $1 -ne 0 ]
   then
-   echo "ERROR :: $2 ... failed"
+   echo -e  "ERROR :: $2 ... $R failed $N"
    exit 1
   else
-   echo "$2 ... success"
+   echo  -e "$2 ... $G success $N "
   fi
 }
 if [ $ID -ne 0 ]
 
 then
-  echo "ERROR :: please run this srcipt with root user"
+  echo -e  "$R ERROR :: please run this srcipt with root user $N"
   exit 1
 else
-     echo "you are root user"
+     echo  "you are root user"
 fi
 
 yum install mysql -y &>> $LOGFILE
