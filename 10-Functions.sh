67f91ea4fb08cc396347007959d64f2aad3669e9
sunil7249 <devops.bsg@gmail.com>
2024-07-21 13:56:55 +0530

    conditions

--- 10-Functions.sh
+++ 10-Functions.sh
@@ -12,6 +12,14 @@ VALIDATE(){
    echo "$2 ... success"
   fi
 }
+if [ $ID -ne 0 ]
+
+then
+  echo "ERROR :: please run this srcipt with root user"
+  exit 1
+else
+     echo "you are root user"
+fi
 
 yum install mysql -y
 
