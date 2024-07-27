dd39552137438ca92c8086b94e67839749fc4cce
sunil7249 <devops.bsg@gmail.com>
2024-07-27 15:59:41 +0530

    sunil script

---
+++ 13-Install packages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+if [ $ID -ne 0 ]
+
+then
+  echo -e  "$R ERROR :: please run this srcipt with root user $N"
+  exit 1
+else
+     echo  "you are root user"
+fi
+
+ echo "All argumnets are passed: $@"
