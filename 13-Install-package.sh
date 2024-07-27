e0ca85c82c565bdd905bca77b4962959637b8ba8
sunil7249 <devops.bsg@gmail.com>
2024-07-27 17:59:18 +0530

    sunil script

--- 13-Install-package.sh
+++ 13-Install-package.sh
@@ -15,12 +15,12 @@ echo "script is executed at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
 
-  if [ $1 -ne 0]
+  if [ $1 -ne 0 ]
   then
       echo -e "$2 ... $R failed $N"
   else
       echo -e "$2 ... $G Success $N"  
-   fi     
+  fi     
 }
 
 if [ $ID -ne 0 ]
